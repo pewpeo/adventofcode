c4c9b492ed8227f8322958132c1bfb5f899c1e35
pewpeo <2847698+pewpeo@users.noreply.github.com>
2023-12-11 21:10:04 +0100

    Add day 8-2

--- 2023/8/src/index.ts
+++ 2023/8/src/index.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { exit } from 'node:process';
+import { lcm } from 'mathjs'
 
 type Node = {
     [id: string] : string;
@@ -36,6 +37,7 @@ try {
 // console.log(instructions);
 // console.log(nodeMap);
 
+// part 1
 let steps = 0;
 let currentNodeId = "AAA"
 let currentInstructionId = 0;
@@ -47,3 +49,23 @@ while (currentNodeId != 'ZZZ') {
 }
 
 console.log("Steps:", steps);
+
+
+// part 2
+let currentNodeIdList: string[] = Object.keys(nodeMap).filter(node => node.endsWith('A'));
+
+// find first end for every start point
+const stepList = currentNodeIdList.map(currentNodeId => {
+    let steps = 0;
+    let currentInstructionId = 0;
+    
+    while (!currentNodeId.endsWith('Z')) {
+        currentNodeId = nodeMap[currentNodeId][instructions[currentInstructionId]];
+        currentInstructionId = (currentInstructionId + 1) % instructions.length;
+        steps++;
+    }
+    return steps;
+});
+
+// has to be the least common multiplier since at some point a node will go in circles
+console.log("Steps 2:", stepList.reduce((a, b) => lcm(a, b)));
